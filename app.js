458fe846c0b2d69bfc0ef92593a4595754eb59e3
Hasnain Amir <hasnain-amir@outlook.com>
2026-03-26 20:05:45 +0000

    test

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 );
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || 'change-this-secret',
     resave: false,
     saveUninitialized: true
 }));
